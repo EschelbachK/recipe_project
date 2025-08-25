f6d062412b72ccaf786736b144fafcf0a2ccdf68
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-08-25 19:23:28 +0200

    add RecipeRepository

--- backend/src/main/java/com/ecosystem/backend/repository/RecipeRepository.java
+++ backend/src/main/java/com/ecosystem/backend/repository/RecipeRepository.java
@@ -3,4 +3,5 @@ package com.ecosystem.backend.repository;
 import com.ecosystem.backend.models.Recipe;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RecipeRepository extends MongoRepository<Recipe, String> {}
+public interface RecipeRepository extends MongoRepository<Recipe, String> {
+}
