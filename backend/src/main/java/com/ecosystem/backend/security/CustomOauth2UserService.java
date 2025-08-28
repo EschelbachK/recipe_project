444ef621570ed1cc223aaa1c33b22a54a894206d
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-08-28 19:05:23 +0200

    add CustomOauth2UserService

---
+++ backend/src/main/java/com/ecosystem/backend/security/CustomOauth2UserService.java
@@ -0,0 +1,38 @@
+package com.ecosystem.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOauth2UserService extends DefaultOAuth2UserService {
+
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        appUserRepository.findById(oAuth2User.getName())
+                .ifPresentOrElse(
+                        u -> {},
+                        () -> createAndSaveUser(oAuth2User)
+                );
+
+        return oAuth2User;
+    }
+
+
+    private void createAndSaveUser(OAuth2User oAuth2User) {
+        AppUser newUser = AppUser.builder()
+                .id(oAuth2User.getName())
+                .username(oAuth2User.getAttribute("login"))
+                .avatarUrl(oAuth2User.getAttribute("avatar_url"))
+                .build();
+
+        appUserRepository.save(newUser);
+    }
+}
