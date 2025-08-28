4a31ea3802157f0a6d07b4889a02c07c244ec71e
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-08-28 18:56:53 +0200

    add AuthController with /api/auth/me

---
+++ backend/src/main/java/com/ecosystem/backend/security/AuthController.java
@@ -0,0 +1,21 @@
+package com.ecosystem.backend.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("/me")
+    public AppUser getMe(@AuthenticationPrincipal OAuth2User user) {
+        return new AppUser(
+                user.getName(),
+                user.getAttributes().get("login").toString(),
+                user.getAttributes().get("avatar_url").toString()
+        );
+    }
+}
