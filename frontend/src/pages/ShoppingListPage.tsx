99228d29e399ab48d03d9d2552bbcf55af750398
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-08-29 20:59:11 +0200

    add placeholder pages for favorites and shopping list

---
+++ frontend/src/pages/ShoppingListPage.tsx
@@ -0,0 +1,8 @@
+export default function ShoppingListPage() {
+    return (
+        <div>
+            <h2>Meine Einkaufsliste</h2>
+            <p>Diese Seite ist bald verfügbar!</p>
+        </div>
+    )
+}
