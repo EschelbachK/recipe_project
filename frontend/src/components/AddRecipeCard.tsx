d9ba9b9dea9d9d71d37ab83585a0edacca5dfd4f
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-09-05 12:26:18 +0200

    add AddRecipeCard with plus symbol and navigation and Style CSS

---
+++ frontend/src/components/AddRecipeCard.tsx
@@ -0,0 +1,26 @@
+import "./AddRecipeCard.css"
+import { useNavigate } from "react-router-dom"
+import { routerConfig } from "../router/routerConfig"
+
+export default function AddRecipeCard() {
+    const navigate = useNavigate()
+
+    function handleAction() {
+        navigate(routerConfig.URL.RECIPE_NEW)
+    }
+
+    return (
+        <button
+            type="button"
+            className="recipe-card add-card"
+            onClick={handleAction}
+        >
+            <div className="card-image">
+                <span className="add-symbol">+</span>
+            </div>
+            <div className="card-content">
+                <span>Neues Rezept erstellen</span>
+            </div>
+        </button>
+    )
+}
