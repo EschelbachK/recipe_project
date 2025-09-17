c26b60cf93ee16fbbeed41f27ceab532757b9fd8
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-09-17 19:06:27 +0200

    update imports after recipe components move

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,36 +1,35 @@
 import "./Header.css"
 import type {AppUser} from "../types/types"
 import {routerConfig} from "../router/routerConfig"
+import logo from "../assets/logo.png"
 
 type Props = {
     user: AppUser | null | undefined
 }
 
-export default function Header({user}: Readonly<Props>) {
+export default function Header({ user }: Readonly<Props>) {
     function handleLogout() {
         window.location.href = routerConfig.API.LOGOUT
     }
 
     return (
         <header className="header">
-            <div className="header-left"/>
+            <div className="header-left" />
             <div className="header-center">
-                <h1>Meine Rezeptübersicht</h1>
+                <img src={logo} alt="RecipelyApp Logo" className="header-logo" />
             </div>
             <div className="header-right">
                 {user ? (
                     <>
                         <span className="greeting">Hallo, {user.username}! 👋</span>
                         {user.avatarUrl && (
-                            <img src={user.avatarUrl} alt="avatar" className="avatar"/>
+                            <img src={user.avatarUrl} alt="avatar" className="avatar" />
                         )}
                         <button className="logout-btn" onClick={handleLogout}>
                             Logout
                         </button>
                     </>
-                ) : (""
-
-                )}
+                ) : null}
             </div>
         </header>
     )
