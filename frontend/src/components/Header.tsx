d4b770f5fbd51543267998725f2f92b2aff22180
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-09-17 19:50:02 +0200

    Replace static title with logo and modernize greeting layout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,23 +7,25 @@ type Props = {
     user: AppUser | null | undefined
 }
 
-export default function Header({ user }: Readonly<Props>) {
+export default function Header({user}: Readonly<Props>) {
     function handleLogout() {
         window.location.href = routerConfig.API.LOGOUT
     }
 
     return (
         <header className="header">
-            <div className="header-left" />
+            <div className="header-left"/>
             <div className="header-center">
-                <img src={logo} alt="RecipelyApp Logo" className="header-logo" />
+                <img src={logo} alt="RecipelyApp Logo" className="header-logo"/>
             </div>
             <div className="header-right">
                 {user ? (
                     <>
-                        <span className="greeting">Hallo, {user.username}! 👋</span>
+                        <span className="greeting">
+                            👋 Hallo, {user.username}
+                        </span>
                         {user.avatarUrl && (
-                            <img src={user.avatarUrl} alt="avatar" className="avatar" />
+                            <img src={user.avatarUrl} alt="avatar" className="avatar"/>
                         )}
                         <button className="logout-btn" onClick={handleLogout}>
                             Logout
