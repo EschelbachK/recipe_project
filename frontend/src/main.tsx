ff667ffc775b6404b66c334b6c97f91f5b7eec43
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-09-17 16:06:56 +0200

    update main.tsx with ToastProvider wrap App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import {StrictMode} from "react"
+import {createRoot} from "react-dom/client"
+import {BrowserRouter} from "react-router-dom"
 
 import "./styles/variables.css"
 import "./styles/global.css"
 
 import App from "./App"
+import {ToastProvider} from "./components/ToastContext.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <ToastProvider>
+                <App/>
+            </ToastProvider>
         </BrowserRouter>
     </StrictMode>
 )
